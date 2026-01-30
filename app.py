3d199b4476a974b2062efd996184d939da11ba42
hbelisario-dbacks <hugojbelisario@gmail.com>
2026-01-29 21:53:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -798,13 +798,13 @@ with tab1:
 
                 if fp_frame is not None:
                     knee_window = df_knee[
-                        (df_knee["frame"] >= fp_frame - 90) &
+                        (df_knee["frame"] >= fp_frame - 80) &
                         (df_knee["frame"] < fp_frame)
                         ]
                 else:
                     # fallback (rare)
                     knee_window = df_knee[
-                        (df_knee["frame"] >= drive_start_frame - 90) &
+                        (df_knee["frame"] >= drive_start_frame - 80) &
                         (df_knee["frame"] < drive_start_frame)
                         ]
             else:
