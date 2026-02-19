5452f3dd5c7a9ddd19804e5f62f4b3b8a27ec088
hbelisario-dbacks <hugojbelisario@gmail.com>
2026-02-18 23:39:42 -0700

    Max Pelvis Angle.

--- app.py
+++ app.py
@@ -3086,8 +3086,8 @@ with tab3:
             if fp_anchor is not None:
                 fpf = int(fp_anchor)
                 mask = (
-                    (frames >= fpf - 30) &
-                    (frames <= fpf + 30)
+                    (frames >= fpf - 60) &
+                    (frames <= fpf + 20)
                 )
                 if np.any(mask):
                     z_vals = z_vals[mask]
